9a918629f810787471db3c9f7aa23ed681c9610c
ExpectTurbulence <ExpectTurbulence@gmail.com>
2020-05-03 16:40:12 -0400

    Propagate open state of vertically stacked windows

--- src/main/java/turbulence/flatpack/blocks/Window.java
+++ src/main/java/turbulence/flatpack/blocks/Window.java
@@ -11,10 +11,13 @@ import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
+import turbulence.flatpack.Registration;
 
 public class Window extends Block {
 
@@ -45,6 +48,17 @@ public class Window extends Block {
     @Override
     public BlockState getStateForPlacement(BlockItemUseContext context) {
         return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing());
+    }
+
+    @Override
+    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn,
+            BlockPos currentPos, BlockPos facingPos) {
+        if (facingState.getBlock() == Registration.WINDOW && (facing == Direction.UP || facing == Direction.DOWN)) {
+            if (facing == Direction.UP) {/*TODO*/}
+            if (facing == Direction.DOWN) {/*TODO*/}
+            return stateIn.with(IS_OPEN, facingState.get(IS_OPEN));
+        }
+        return stateIn;
     }
 
     @Override
