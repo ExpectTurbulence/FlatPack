64a3a8b10fa915110d54e8d6e51c4cd9b2a8f4f1
ExpectTurbulence <ExpectTurbulence@gmail.com>
2020-05-03 16:24:37 -0400

    Create and register window block

--- src/main/java/turbulence/flatpack/Registration.java
+++ src/main/java/turbulence/flatpack/Registration.java
@@ -6,18 +6,24 @@ import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.registries.ObjectHolder;
 import turbulence.flatpack.blocks.TestBlock;
+import turbulence.flatpack.blocks.Window;
 
 public class Registration {
 
     @ObjectHolder(FlatPack.modid + ":" + TestBlock.registryName)
     public static TestBlock TESTBLOCK;
 
+    @ObjectHolder(FlatPack.modid + ":" + Window.registryName)
+    public static Window WINDOW;
+
     public static void registerBlocks(final RegistryEvent.Register<Block> event) {
         event.getRegistry().register(new TestBlock());
+        event.getRegistry().register(new Window());
     }
     
     public static void registerItems(final RegistryEvent.Register<Item> event) {
         registerBlockItem(TESTBLOCK, event);
+        registerBlockItem(WINDOW, event);
     }
 
     public static void registerBlockItem(Block block, final RegistryEvent.Register<Item> event) {
---
+++ src/main/java/turbulence/flatpack/blocks/Window.java
@@ -0,0 +1,56 @@
+package turbulence.flatpack.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.World;
+
+public class Window extends Block {
+
+    final public static String registryName = "window";
+
+    public static final BooleanProperty IS_OPEN = BooleanProperty.create("is_open");
+    public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+
+    public Window() {
+        super(Properties.create(Material.GLASS)
+            .sound(SoundType.GLASS)
+            .hardnessAndResistance(1.0f)
+        );
+        setRegistryName(registryName);
+        this.setDefaultState(this.stateContainer.getBaseState().with(IS_OPEN, false));
+    }
+
+    @Override
+    public BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    @Override
+    protected void fillStateContainer(Builder<Block, BlockState> builder) {
+        builder.add(FACING, IS_OPEN);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing());
+    }
+
+    @Override
+    public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn,
+            BlockRayTraceResult hit) {
+        worldIn.setBlockState(pos, state.with(IS_OPEN, !state.get(IS_OPEN)), 1);
+        return true;
+    }
+}
